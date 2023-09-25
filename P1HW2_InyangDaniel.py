1993f258edfa529c85164909c466640d23347156
inyangd0932 <145804986+inyangd0932@users.noreply.github.com>
2023-09-25 15:23:23 -0400

    Add files via upload
    Third submission

---
+++ P1HW2_InyangDaniel.py
@@ -0,0 +1,46 @@
+# This program calculates and displays travel expenses 
+
+# 25 Sept 23
+
+# CTI-110 P1HW2 - Travel Expense
+
+# Daniel Inyang
+
+#
+
+print('This program calculates and displays travel expenses')
+print()
+print('Enter Budget:', int(2000))
+print()
+print('Enter your travel destination:', 'Hawaii')
+print()
+print('How much do you think you will spend on your flight?',int(600))
+print()
+print('Approximatley, how much will you need for accomodation/hotel?', int(800))
+print()
+print('Last, how much do you need for food?', int(400))
+print()
+print('------------Travel Expenses------------')
+
+print('Location: Hawaii')
+print('Initial Budget:', int(2000))
+
+print()
+
+print('Flight:', int(600))
+print('Accomodation:', int(800))
+print('Food:', int(400))
+print()
+print('Remaining Balance:', int(200))
+
+
+# Set max budget to 2000
+# Determine the desired destination
+# Determine the cost of the flight to destination
+# Determine the amount that will be spent on hotel fees
+# Estimate the amount that would be used for food
+
+      
+      
+      
+
