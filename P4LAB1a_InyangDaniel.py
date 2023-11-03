3db1265e8fb2c63de863ff790ba162172705caeb
inyangd0932 <145804986+inyangd0932@users.noreply.github.com>
2023-11-03 13:30:51 -0400

    Add files via upload
    Eleventh submission

---
+++ P4LAB1a_InyangDaniel.py
@@ -0,0 +1,12 @@
+import turtle
+Sam = turtle.Turtle()
+
+for x in range(4):
+    Sam.forward(100)
+    Sam.left(90)
+
+    
+    
+for x in range(3):
+    Sam.forward(100)
+    Sam.left(120)
---
+++ P4LAB1b_InyangDaniel.py
@@ -0,0 +1,34 @@
+import turtle
+Sam = turtle.Turtle()
+
+Sam.color("green")
+Sam.pensize(3)
+
+
+Sam.right(90)
+Sam.forward(100)
+Sam.left(135)
+Sam.forward(30)
+Sam.left(45)
+Sam.forward(60)
+Sam.left(45)
+Sam.forward(30)
+
+Sam.penup()
+Sam.goto(50,0)
+Sam.pendown()
+
+
+Sam.right(45)
+Sam.right(90)
+Sam.forward(50)
+Sam.left(180)
+Sam.forward(25)
+Sam.left(90)
+Sam.forward(100)
+Sam.left(90)
+Sam.forward(25)
+Sam.right(180)
+Sam.forward(50)
+
+
