e6ae64bc1b0b1fae64a5d492dd234adbf64eeeb7
inyangd0932 <145804986+inyangd0932@users.noreply.github.com>
2023-12-09 16:09:19 -0500

    Update P5LAB_InyangDaniel.py

--- P5LAB_InyangDaniel.py
+++ P5LAB_InyangDaniel.py
@@ -7,8 +7,18 @@ def days_in_feb(user_year):
         return 28
 
 if __name__ == '__main__':
-    year = int(input())
-    day = days_in_feb(int(year))
-    print(f'{year} has {day} days in February.')
+    while True:
+        try:
+            input_years = input("Enter years separated by commas (or type 'exit' to end): ")
+            if input_years.lower() == 'exit':
+                break  
+            
+            years = [int(year.strip()) for year in input_years.split(',')]
+            
+            for year in years:
+                day = days_in_feb(year)
+                print(f'{year} has {day} days in February.')
+        except ValueError:
+            print("Invalid input. Please enter valid years or 'exit'.")
 
     
